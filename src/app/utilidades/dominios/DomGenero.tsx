93353852191feac849613d6dd69b8401487b9330
jsandova26 <jsandova26@cuc.edu.co>
2024-05-14 16:21:06 -0500

    m1

---
+++ src/app/utilidades/dominios/DomGenero.tsx
@@ -0,0 +1,9 @@
+import { PeliculaGenero } from "../../modelos/PeliculaGenero";
+
+export const ARREGLO_PELICULA_GENERO: Array<PeliculaGenero> = [
+  { codGenero: "1", nombreGenero: "Accion" },
+  { codGenero: "2", nombreGenero: "Drama" },
+  { codGenero: "3", nombreGenero: "Ciencia-Ficción" },
+  { codGenero: "4", nombreGenero: "Terror" },
+  { codGenero: "5", nombreGenero: "Comedia" },
+];
